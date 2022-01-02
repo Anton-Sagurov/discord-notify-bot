8ec7c1514c20855c6d18c544d48da8f24a20815d
Anton Sagurov <anton.sagurov@gmail.com>
2022-01-02 16:05:59 +0300

    Auzilarity script for build and push Docker image created

---
+++ build_push.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+IMAGE_NAME='sagurov/discord-notification-bot'
+VERSION=$(<./VERSION )
+
+if [ -z "$VERSION" ];then
+  "$VERSION is not defined"
+  exit 1
+fi
+
+docker build . -t "$IMAGE_NAME:$VERSION"
+docker push "$IMAGE_NAME:$VERSION"
